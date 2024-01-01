1e6a29fcd43b0611da7113170330b830cf25421c
Ryan Alli <ryanalli77@gmail.com>
2024-01-01 16:14:47 -0500

    added variable sizing

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
 const etchBody = document.querySelector('.etch-body');
-
-
-const tiles = [];
-
-for(let i = 0; i < 64; i ++)
+let num = 16
+drawGrid();
+document.getElementById("submit").onclick = function()
 {
-  let ecthRowContainer = document.createElement('div');
-  ecthRowContainer.classList.add('row')
-  for(let j = 0; j < 64; j ++)
-  {
-    let tile = document.createElement('span');
-    tile.classList.add('etch-tile');
-    ecthRowContainer.appendChild(tile);
-    tile.addEventListener('mouseover', function()
-    {
-      tile.style.background = 'black';
-    });
+  while (etchBody.lastElementChild) {
+    etchBody.removeChild(etchBody.lastElementChild);
   }
-  etchBody.appendChild(ecthRowContainer)
+  num = Number(document.getElementById("input").value)
+  if(num > 100)
+    num = 100;
+  drawGrid();
+};
+
+function drawGrid()
+{
+  for(let i = 0; i < num; i ++)
+  {
+    let ecthRowContainer = document.createElement('div');
+    ecthRowContainer.classList.add('row')
+    for(let j = 0; j < num; j ++)
+    {
+      let tile = document.createElement('span');
+      let pading = ((800 / num) - 2) / 2
+      tile.style.padding = pading.toString()+"px"
+      tile.classList.add('etch-tile');
+      ecthRowContainer.appendChild(tile);
+      tile.addEventListener('mouseover', function()
+      {
+        tile.style.background = 'black';
+      });
+    }
+    etchBody.appendChild(ecthRowContainer)
+  }
 }
-
-
-
-
 

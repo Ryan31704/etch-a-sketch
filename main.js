669fbcefb1c0e9926a094820a62b19d5755ee7a3
Ryan Alli <ryanalli77@gmail.com>
2024-01-01 16:21:27 -0500

    comments and documentation

--- main.js
+++ main.js
@@ -16,21 +16,27 @@ function drawGrid()
 {
   for(let i = 0; i < num; i ++)
   {
+    //container for all boxes in a row
     let ecthRowContainer = document.createElement('div');
     ecthRowContainer.classList.add('row')
     for(let j = 0; j < num; j ++)
     {
+      //created tiles as spans so they are inline
       let tile = document.createElement('span');
+      //calculates padding based on max width and accounts for border sizing
       let pading = ((800 / num) - 2) / 2
       tile.style.padding = pading.toString()+"px"
+
       tile.classList.add('etch-tile');
+      //appends tiles into row container div
       ecthRowContainer.appendChild(tile);
       tile.addEventListener('mouseover', function()
       {
         tile.style.background = 'black';
       });
     }
-    etchBody.appendChild(ecthRowContainer)
+    //each row container div is appended to the body so we have n number row divs containing n number of span tiles
+    etchBody.appendChild(ecthRowContainer);
   }
 }
 
